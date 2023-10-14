7f5e9ba037c02985d79ba4e23e407b54d8b02c0b
Rao Fahad <raofahad046@gmail.com>
2023-10-14 06:24:40 +0500

    completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,83 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+import { useEffect, useState } from "react";
+import AWS from "aws-sdk";
+import axios from "axios";
 
 export default function Home() {
+  AWS.config.update({
+    accessKeyId: "AKIAQHLGGFVPLYP5JOXA",
+    secretAccessKey: "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",
+    region: "us-east-1",
+  });
+  const [image, setImage] = useState<any>();
+  const formData = new FormData();
+  formData.append("file", image);
+
+  // const postImage = async () => {
+  //   const presignedUrl = await fetch(
+  //     "https://i33z4hwmv4.execute-api.us-east-1.amazonaws.com/get-presigned-url",
+  //     {
+  //       method: "POST",
+  //       body: JSON.stringify({
+  //         name: `store-logos/${image.name}`,
+  //         fileType: image.type,
+  //       }),
+  //     }
+  //   );
+
+  //   const res = await presignedUrl.json();
+  //   console.log("url first", res);
+  //   await fetch(res.url, {
+  //     headers: {
+  //       "Content-Type": image.type,
+  //     },
+  //     method: "PUT",
+  //     body: formData,
+  //   }).then((response) => {
+  //     console.log("called second");
+  //   });
+  // };
+
+  const uploadToS3 = async (file: any, isLogo: any) => {
+    try {
+      const s3 = new AWS.S3();
+      const bucketName = "livecart-store-api-dev-store-images-bucket";
+
+      const params = {
+        Bucket: bucketName,
+        Key: isLogo ? `store-logos/${file.name}` : `store-photos/${file.name}`,
+        ContentType: file.type,
+      };
+
+      const presignedUrl = await s3.getSignedUrlPromise("putObject", params);
+
+      await axios.put(presignedUrl, file, {
+        headers: {
+          "Content-Type": file.type,
+        },
+      });
+
+      const imageUrl = `https://${bucketName}.s3.amazonaws.com/${params.Key}`;
+      return imageUrl;
+    } catch (error) {
+      console.error("Error uploading to S3:", error);
+      throw error;
+    }
+  };
+
+  useEffect(() => {
+    uploadToS3(image, true);
+  }, [image]);
+
+  // useEffect(() => {
+  //   if (image) {
+  //     console.log("image object=>", image);
+  //     postImage();
+  //   }
+  // }, [image]);
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    <>
+      <input type="file" onChange={(e: any) => setImage(e.target.files[0])} />
+    </>
+  );
 }
